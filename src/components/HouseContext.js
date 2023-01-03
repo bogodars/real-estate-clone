021f5163c9e65fd67e659150cbd484bd7d193962
Bogodar Shmyglyk <bogodar@gmail.com>
2023-01-03 23:42:33 +0200

    first commit

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+import { housesData } from '../data';
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState('Location (any)');
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState('Property type (any) ');
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState('Price range (any) ');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+    console.log(allCountries);
+    const uniqueCountries = ['Location (any)', ...new Set(allCountries)];
+    console.log(uniqueCountries);
+    setCountries(uniqueCountries);
+  }, []);
+
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        countries,
+        setCountry,
+        setCountries,
+        setPrice,
+        price,
+        loading,
+        houses,
+        properties,
+        property,
+        properties,
+      }}>
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
